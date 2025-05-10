c3602be24ad60183e7d165002ed4b66cec25cab5
Ganesh Gawali <106748963+GawaliGanesh123@users.noreply.github.com>
2025-05-10 22:57:29 +0530

    Print_Even_Numbers.c

--- Print_Even_Numbers.c
+++ Print_Even_Numbers.c
@@ -1,9 +1,9 @@
-/*
-    Accept One Number from user and print  all Even Numbers between 1 to User given number.
+/* Accept One Number from user and print  all Even Numbers between 1 to User given number.
 
     Input-30
     Ouput- 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30
 */
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 #include<stdio.h>
